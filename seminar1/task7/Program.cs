64ed32b9793c14ee11d8ac781532cbcf016ab508
r0acho <ivan.linzov@gmail.com>
2022-08-30 23:53:46 +0300

    fixed task 7

--- seminar1/task7/Program.cs
+++ seminar1/task7/Program.cs
@@ -6,10 +6,11 @@
 
 Console.WriteLine("Введите трехзначное число:");
 int number = Convert.ToInt32(Console.ReadLine());
+
 if (number > 99 && number < 1000)
 {
-    double count = number / 100.0;
-    Console.WriteLine(count);
+    int digit = number % 10;
+    Console.WriteLine(digit);
 }
 else
 {
