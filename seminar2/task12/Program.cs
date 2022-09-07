e5fb6f74d68b922e22b10457934755270912df52
r0acho <ivan.linzov@gmail.com>
2022-09-07 17:33:44 +0300

    seminar3 tasks had pushed

--- seminar2/task12/Program.cs
+++ seminar2/task12/Program.cs
@@ -1,28 +1,9 @@
-﻿// Задача 12: Напишите программу, которая будет принимать на 
-// вход два числа и выводить, является ли первое число кратным второму. 
-// Если число 1 не кратно числу 2, то программа выводит остаток от деления.
-// 34, 5 -> не кратно, остаток 4
-// 16, 4 -> кратно
-
-Console.WriteLine("Введите первое число");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите второе число");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-
-if (secondNumber != 0)
-{
-    int mod = firstNumber % secondNumber;
-    if (mod == 0)
-    {
-        Console.WriteLine("кратно");
-    }
-    else
-    {
-        Console.WriteLine($"не кратно, остаток {mod}");
-    }
+﻿int chooseNum(int num){
+if(num>=100){
+    int result = num%100-(num%100)%10;
+    return result;
 }
-else
-{
-    Console.WriteLine("Нельзя делить на 0!");
+else return 0;
 }
+
+Console.WriteLine(chooseNum(Convert.ToInt32(Console.Read())));
