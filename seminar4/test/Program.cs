548eaf63eae1223a56360719961d896aea990264
r0acho <ivan.linzov@gmail.com>
2022-09-09 21:33:35 +0300

    task 26 realized

---
+++ seminar4/test/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+if (number > 1)
+{
+    for (int i = 1; number > 0; i++)
+    {
+        number = number / 10;
+        x++;
+    }
+    Console.Write($"Количество цифр в числе состовляет {x}");
+}
+else
+{
+    Console.Write("Вы ввели некорректное число.");
+}
