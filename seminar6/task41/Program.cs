c332bb5d6b8c140f93f72a662a2f9a376224ed71
r0acho <ivan.linzov@gmail.com>
2022-09-20 20:51:13 +0300

    added task 46

---
+++ seminar6/task41/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int countOfPositive = 0;
+
+while (true)
+{
+    string answer = Console.ReadLine();
+    if (answer  == "stop") // если ответ пользователья - стоп слово
+    {
+        break;
+    }
+    else
+    {
+        double.TryParse(answer, out double answerNumber);
+        if (answerNumber > 0)
+        {
+            countOfPositive++;
+        }
+    }
+}
+Console.WriteLine(countOfPositive);
