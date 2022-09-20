e62ca8021d5c211463e1ffd8481f85180812f123
r0acho <ivan.linzov@gmail.com>
2022-09-20 21:06:07 +0300

    added task 48

---
+++ seminar7/test/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] GetArray(int size,int leftRange,  int rightRange) // 5
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = i;
+    }
+    return arr;
+}
+int [] arr = GetArray(12, -9, 9);
+Console.Write("Сгенерирован массив: ");
+Console.WriteLine(string.Join(", ", arr));
